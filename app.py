4a552a7491905fab97e0243735cdcde2bbcef64c
Travis-Berthrong <33703020+Travis-Berthrong@users.noreply.github.com>
2023-12-09 18:20:33 +0100

    Implemented login & sign up routes

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, url_for, redirect
 from dotenv import load_dotenv
 import os
+from flask_login import LoginManager
 
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+@app.route('/')
+def index():
+    return redirect(url_for('authentication.login'))
 
 
 if __name__ == '__main__':
--- authentication/views.py
+++ authentication/views.py
@@ -1,17 +1,18 @@
 import sqlite3
-from flask import g, render_template, redirect, url_for, flash
+from flask import g, render_template, redirect, url_for, flash, session, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from . import authentication
 
-from final_project.app import app
-
 login_manager = LoginManager()
 login_manager.login_view = "authentication.login"
 login_manager.login_message = 'Please log in to access this page.'
-login_manager.init_app(app)
+
+@authentication.record_once
+def on_load(state):
+    login_manager.init_app(state.app)
 
 class User(UserMixin):
     def __init__(self, id, username, password, email, is_driver=False):
@@ -22,71 +23,81 @@ class User(UserMixin):
         self.is_driver = is_driver
 
 @login_manager.user_loader
-def load_user(user_id, get_db, table):
+def load_user(user_id):
     db = get_db()
     cursor = db.cursor()
-    cursor.execute(f'SELECT * FROM {table} WHERE id = ?', (user_id,))
-    user = cursor.fetchone()
-    if user:
-        if table == 'drivers':
+    is_driver = session.get('is_driver', False)
+    if is_driver:
+        cursor.execute(f'SELECT * FROM drivers WHERE id = ?', (user_id,))
+        user = cursor.fetchone()
+        if user:
             return User(user[0], user[1], user[2], user[3], is_driver=True)
-        return User(user[0], user[1], user[2], user[3])
     else:
-        return None
+        cursor.execute(f'SELECT * FROM clients WHERE id = ?', (user_id,))
+        user = cursor.fetchone()
+        if user:
+            return User(user[0], user[1], user[2], user[3])
+    return None
 
-def get_clients_db():
-    db = getattr(g, '_clients_database', None)
+def get_db():
+    db = getattr(g, '_database', None)
     if db is None:
-        db = g._users_database = sqlite3.connect('./clients.db')
+        db = g._users_database = sqlite3.connect('./uber_application.db')
         create_clients_table(db)
-    return db
-
-def create_clients_table(db):
-    cursor = db.cursor()
-    cursor.execute('CREATE TABLE IF NOT EXISTS clients (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, email TEXT)') 
-    db.commit()
-    cursor.close()
-
-def get_drivers_db():
-    db = getattr(g, '_drivers_database', None)
-    if db is None:
-        db = g._drivers_database = sqlite3.connect('./drivers.db')
         create_drivers_table(db)
     return db
 
+# need to change tables to ensure unique Ids
+def create_clients_table(db):
+    cursor = db.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS clients (id INTEGER PRIMARY KEY, username TEXT, password TEXT, email TEXT)') 
+    cursor.execute
+    db.commit()
+    cursor.close()
+
 def create_drivers_table(db):
     cursor = db.cursor()
-    cursor.execute('CREATE TABLE IF NOT EXISTS drivers (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, email TEXT, first_name TEXT, last_name TEXT, phone_number TEXT, vehicle TEXT, license_plate TEXT, available BOOLEAN)')
+    cursor.execute('CREATE TABLE IF NOT EXISTS drivers (id INTEGER PRIMARY KEY, username TEXT, password TEXT, email TEXT, first_name TEXT, last_name TEXT, phone_number TEXT, vehicle TEXT, license_plate TEXT, available BOOLEAN)')
+    db.commit()
+    cursor.close()
 
 
-def unique_username(form, field, get_db, table):
+def unique_username(form, field):
     db = get_db()
     cursor = db.cursor()
-    cursor.execute(f'SELECT * FROM {table} WHERE username = ?', (field.data,))
-    current_user = cursor.fetchone()
-    if current_user:
+    cursor.execute(f'SELECT * FROM clients WHERE username = ?', (field.data,))
+    current_user1 = cursor.fetchone()
+    cursor.execute(f'SELECT * FROM drivers WHERE username = ?', (field.data,))
+    current_user2 = cursor.fetchone()
+    if current_user1 or current_user2:
         raise ValidationError('Username already exists')
     
-def unique_email(form, field, get_db, table):
+def unique_email(form, field):
+    # check if request originated from client or driver sign up form
+    is_driver = form.vehicle.data is not None
     db = get_db()
     cursor = db.cursor()
-    cursor.execute(f'SELECT * FROM {table} WHERE email = ?', (field.data,))
-    current_user = cursor.fetchone()
+    if is_driver:
+        cursor.execute(f'SELECT * FROM drivers WHERE email = ?', (field.data,))
+        current_user = cursor.fetchone()
+    else:
+        cursor.execute(f'SELECT * FROM clients WHERE email = ?', (field.data,))
+        current_user = cursor.fetchone()
     if current_user:
         raise ValidationError('Account already exists')
 
 class ClientRegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20), unique_username(get_db=get_clients_db, table='clients')])
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=4, max=20)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    email = StringField('Email', validators=[DataRequired(), Email(), Length(min=4, max=20), unique_email(get_db=get_clients_db, table='clients')])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(min=4, max=20)])
     submit = SubmitField('Sign Up')
 
 class DriverRegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20), unique_username(get_db=get_drivers_db, table='drivers')])
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=4, max=20)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    email = StringField('Email', validators=[DataRequired(), Email(), Length(min=4, max=20), unique_email(get_db=get_drivers_db, table='drivers')])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(min=4, max=20)])
     first_name = StringField('First Name', validators=[DataRequired(), Length(min=4, max=20)])
     last_name = StringField('Last Name', validators=[DataRequired(), Length(min=4, max=20)])
     phone_number = StringField('Phone Number', validators=[DataRequired(), Length(min=4, max=20)])
@@ -125,18 +136,21 @@ def client_login():
         username = form.username.data
         password = form.password.data
         
-        db = get_clients_db()
+        db = get_db()
         cursor = db.cursor()
         cursor.execute('SELECT * FROM clients WHERE username = ? AND password = ?', (username, password))
         user = cursor.fetchone()
         cursor.close()
         if user:
             user_obj = User(user[0], user[1], user[2], user[3])
+            session['is_driver'] = False
             login_user(user_obj)
             return redirect(url_for('clients.home'))
         else:
             flash('Invalid username or password', 'danger')
     return render_template('client_login.html', form=form)
+
+
 @authentication.route('/login/driver', methods=['GET', 'POST'])
 def driver_login():
     if current_user.is_authenticated:
@@ -148,14 +162,14 @@ def driver_login():
     if form.validate_on_submit():
         username = form.username.data
         password = form.password.data
-        
-        db = get_drivers_db()
+        db = get_db()
         cursor = db.cursor()
         cursor.execute('SELECT * FROM drivers WHERE username = ? AND password = ?', (username, password))
         user = cursor.fetchone()
         cursor.close()
         if user:
             user_obj = User(user[0], user[1], user[2], user[3], is_driver=True)
+            session['is_driver'] = True
             login_user(user_obj)
             return redirect(url_for('drivers.home'))
         else:
@@ -164,18 +178,13 @@ def driver_login():
 
 @authentication.route('/sign_up/client', methods=['GET', 'POST'])
 def client_sign_up():
-    if current_user.is_authenticated:
-        if current_user.is_driver:
-            return redirect(url_for('drivers.home'))
-        return redirect(url_for('clients.home'))
-    
     form = ClientRegistrationForm()
     if form.validate_on_submit():
         username = form.username.data
         password = form.password.data
         email = form.email.data
         
-        db = get_clients_db()
+        db = get_db()
         cursor = db.cursor()
         cursor.execute('SELECT * FROM clients WHERE username = ?', (username,))
         current_user = cursor.fetchone()
@@ -191,11 +200,6 @@ def client_sign_up():
 
 @authentication.route('/sign_up/driver', methods=['GET', 'POST'])
 def driver_sign_up():
-    if current_user.is_authenticated:
-        if current_user.is_driver:
-            return redirect(url_for('drivers.home'))
-        return redirect(url_for('clients.home'))
-    
     form = DriverRegistrationForm()
     if form.validate_on_submit():
         username = form.username.data
@@ -207,7 +211,7 @@ def driver_sign_up():
         vehicle = form.vehicle.data
         license_plate = form.license_plate.data
         
-        db = get_drivers_db()
+        db = get_db()
         cursor = db.cursor()
         cursor.execute('SELECT * FROM drivers WHERE username = ?', (username,))
         current_user = cursor.fetchone()
